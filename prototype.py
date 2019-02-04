64da0904ccd1a484726129b644c004bb79f2244b
kemfic <19ficici.kemal@gmail.com>
2019-02-04 14:07:42 -0500

    vision display

--- prototype.py
+++ prototype.py
@@ -2,6 +2,13 @@ import cv2
 import numpy as np
 
 if __name__ == '__main__':
-  cap = cv2.VideoCapture(1)
-  ret, frame = cap.get()
-  cv2.imshow('out', frame)
+  cap = cv2.VideoCapture(0)
+  while True:
+    ret, frame = cap.read()
+    cv2.imshow('frame', frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+      break
+
+  cap.release()
+  cv2.destroyAllWindows()
