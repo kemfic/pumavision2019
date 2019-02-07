ac221a332b4baf86e1c546f55ddd5f253a3fac5d
kemfic <19ficici.kemal@gmail.com>
2019-02-06 20:41:00 -0500

    calibrated image thresholds

--- prototype.py
+++ prototype.py
@@ -2,20 +2,20 @@ import cv2
 import numpy as np
 k_scale = 0.5
 
-h_thresh = (50, 90)
-s_thresh = (140, 255)
-v_thresh = (140, 255)
+h_thresh = (70, 90)
+s_thresh = (120, 255)
+v_thresh = (230, 255)
 
 
 def preprocess(img):
   #img = cv2.resize(img, (0,0), k_scale, k_scale)
 
-  kernel = np.ones((7,7), np.uint8)
+  #kernel = np.ones((7,7), np.uint8)
 
-  img = cv2.erode(img, kernel, 10)
-  img = cv2.dilate(img, kernel, 10)
+  #img = cv2.erode(img, kernel, 10)
+  #img = cv2.dilate(img, kernel, 10)
 
-  img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+  img = cv2.cvtColor(img, cv2.COLOR_BGR2HLS)
   return img
 
 def get_mask(img):
@@ -42,11 +42,12 @@ if __name__ == '__main__':
     img = preprocess(frame)
     mask = get_mask(img)
 
+
     cv2.imshow("input", frame)
+    cv2.imshow("mask", mask*255)
 
 
-    cv2.imshow("output", cv2.cvtColor(cv2.bitwise_and(img, img, mask=mask), cv2.COLOR_HSV2BGR))
-
+    cv2.imshow("output", cv2.cvtColor(cv2.bitwise_and(img, img, mask=mask), cv2.COLOR_HLS2BGR))
     if cv2.waitKey(1) & 0xFF == ord('q'):
       break
 
