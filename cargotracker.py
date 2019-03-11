4cfceca6b3a96302b238012b8194b8abbe0f89d7
kemfic <19ficici.kemal@gmail.com>
2019-03-11 15:31:11 -0400

    finished contour detector for cargo, gonna clean up code after lunch :

--- cargotracker.py
+++ cargotracker.py
@@ -8,12 +8,27 @@ class CargoTracker(object):
   def __init_(self):
     self.img = np.zeros((500,500))
   def pipeline(self, img):
-    self.img = cv2.resize(img, (100, 100), cv2.INTER_NEAREST)
+    self.img = cv2.resize(img, (150, 150), cv2.INTER_NEAREST)
     self.img = cv2.cvtColor(self.img, cv2.COLOR_BGR2HLS)
+    
     self.mask = cv2.inRange(self.img, self.min_thresh, self.max_thresh)
-    self.cnt, self.hier = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    
+    kernel = np.ones((5,5), np.uint8)
+    #self.mask = cv2.dilate(self.mask,kernel, iterations=2) 
+    
+    
+    self.cnt, self.hier = cv2.findContours(self.mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    self.cnt = sorted(self.cnt, key=cv2.contourArea, reverse=True)[0] # get largest contour
+
+    M = cv2.moments(self.cnt)
+    cx = int(M['m10']/M['m00'])
+    cy = int(M['m01']/M['m00'])
+    print(cx - self.img.shape[0]//2)
+    print(cx)
     self.ret = np.copy(self.img)
-    self.ret = cv2.drawContours(self.ret, self.cnt, -1, (150, 100, 255), 2)
+    self.ret = cv2.drawContours(self.ret, self.cnt, -1, (150, 100, 255), 3)
+    self.ret = cv2.circle(self.ret, (cx, cy), 2, (150, 100, 255))
+
 if __name__ == "__main__":
   ct = CargoTracker()
   img = cv2.imread('img/0.jpg')
